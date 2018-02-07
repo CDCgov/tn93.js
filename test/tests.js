58d0198d9677edb1913d6a74a06d931763838ac4
Tony Boyles <Anthony@Boyles.cc>
2018-02-07 17:13:35 -0500

    Added some tests
    Really need to engineer some non-trivial cases.

--- test/tests.js
+++ test/tests.js
@@ -5,30 +5,43 @@ var s2 = "CCCATTAGTCCTATTGAAACTGTACCAGTAAAATTAAAGCCAGGAATGGATGGCCCAAAAGTTAAACAAT
 QUnit.test( "Default", assert => {
 	assert.expect(2);
 	var a = tn93(s1, s2);
-	var b = tn93(s1, s2, "RESOLVE", 100);
-	assert.equal(a, b, "Function defaults are correct.");
-	assert.notEqual(a, 1, "Function returns non-trivial results")
-});
-
-QUnit.test( "Resolve", assert => {
-	assert.expect(2);
-	var a = tn93(s1, s2);
 	var b = tn93(s1, s2);
 	assert.equal(a, b, "Function is Stable." );
 	assert.ok(a - 0.045156 < .0000001 && a - 0.045156 > -.0000001, "Function is Correct.");
 });
 
+QUnit.test( "Resolve", assert => {
+	assert.expect(3);
+	var a = tn93(s1, s2, "RESOLVE", 100);
+	var b = tn93(s1, s2, "RESOLVE", 100);
+	assert.equal(a, b, "Function is Stable.");
+	assert.notEqual(a, 1, "Function returns non-trivial results");
+	assert.ok(a - 0.045156 < .0000001 && a - 0.045156 > -.0000001, "Function is Correct.");
+});
+
 QUnit.test( "Average", assert => {
-	assert.expect(1);
-	assert.ok( 1 == "1", "Passed!" );
+	assert.expect(3);
+	var a = tn93(s1, s2, "AVERAGE", 100);
+	var b = tn93(s1, s2, "AVERAGE", 100);
+	assert.equal(a, b, "Function is Stable.");
+	assert.notEqual(a, 1, "Function returns non-trivial results");
+	assert.ok(a - 0.045156 < .0000001 && a - 0.045156 > -.0000001, "Function is Correct.");
 });
 
 QUnit.test( "Skip", assert => {
-	assert.expect(1);
-	assert.ok( 1 == "1", "Passed!" );
+	assert.expect(3);
+	var a = tn93(s1, s2, "SKIP", 100);
+	var b = tn93(s1, s2, "SKIP", 100);
+	assert.equal(a, b, "Function is Stable.");
+	assert.notEqual(a, 1, "Function returns non-trivial results");
+	assert.ok(a - 0.045156 < .0000001 && a - 0.045156 > -.0000001, "Function is Correct.");
 });
 
 QUnit.test( "GapMM", assert => {
-	assert.expect(1);
-	assert.ok( 1 == "1", "Passed!" );
+	assert.expect(3);
+	var a = tn93(s1, s2, "GAPMM", 100);
+	var b = tn93(s1, s2, "GAPMM", 100);
+	assert.equal(a, b, "Function is Stable.");
+	assert.notEqual(a, 1, "Function returns non-trivial results");
+	assert.ok(a - 0.045156 < .0000001 && a - 0.045156 > -.0000001, "Function is Correct.");
 });
